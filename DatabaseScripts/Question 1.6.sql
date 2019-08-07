862da68e4cda548aecc5623a7ee98ed641af23cf
Mathew <seuzrulz@gmail.com>
2019-08-08 01:22:28 +0200

    updated question 1.6 to work

--- DatabaseScripts/Question 1.6.sql
+++ DatabaseScripts/Question 1.6.sql
@@ -1,13 +1,16 @@
 create procedure Question1point6 @StaffID int
 as
 select Nickname, 
-(select InOutStatus.Description from StaffInOutHistory inner join InOutStatus on StaffInOutHistory.InOutStatusIDOld = InOutStatus.InOutStatusID  where StaffInOutHistory.StaffID = @StaffID ) 'Status before change' ,
-(select InOutStatus.Description from StaffInOutHistory inner join InOutStatus on StaffInOutHistory.InOutStatusIDNew = InOutStatus.InOutStatusID  where StaffInOutHistory.StaffID = @StaffID ) 'status after change',
+s1.Description 'Status before change' ,
+s2.Description 'status after change',
 StaffInOutHistory.DateChanged,
-(Select Staff.Nickname from Staff inner join StaffInOutHistory on  Staff.StaffID = StaffInOutHistory.StaffID where StaffInOutHistory.ChangedByStaffID = @StaffID) 'Status change by' 
+StaffInOutHistory.ChangedByStaffID 'Status change by' 
 from Staff
 inner join StaffInOutHistory on Staff.StaffID = StaffInOutHistory.StaffID
+inner join InOutStatus s1 on StaffInOutHistory.InOutStatusIDOld = s1.InOutStatusID
+inner join InOutStatus s2 on StaffInOutHistory.InOutStatusIDNew = s2.InOutStatusID
 where Staff.StaffID = @StaffID
+
 
 
 exec Question1point6 1
